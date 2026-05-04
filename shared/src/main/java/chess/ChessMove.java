13779f3152b54a75827934535780d140d84229de
jbarton0 <jackienomin@gmail.com>
2026-05-04 13:53:47 -0600

    ChessMove fully implemented

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents moving a chess piece on a chessboard
  * <p>
@@ -8,22 +10,29 @@ package chess;
  */
 public class ChessMove {
 
+    private final ChessPosition myStart;
+    private final ChessPosition myEnd;
+    private final ChessPiece.PieceType newPiece;
+
     public ChessMove(ChessPosition startPosition, ChessPosition endPosition,
                      ChessPiece.PieceType promotionPiece) {
+        myStart = startPosition;
+        myEnd = endPosition;
+        newPiece = promotionPiece;
     }
 
     /**
      * @return ChessPosition of starting location
      */
     public ChessPosition getStartPosition() {
-        throw new RuntimeException("Not implemented");
+        return myStart;
     }
 
     /**
      * @return ChessPosition of ending location
      */
     public ChessPosition getEndPosition() {
-        throw new RuntimeException("Not implemented");
+        return myEnd;
     }
 
     /**
@@ -33,6 +42,20 @@ public class ChessMove {
      * @return Type of piece to promote a pawn to, or null if no promotion
      */
     public ChessPiece.PieceType getPromotionPiece() {
-        throw new RuntimeException("Not implemented");
+        return newPiece;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessMove chessMove = (ChessMove) o;
+        return Objects.equals(myStart, chessMove.myStart) && Objects.equals(myEnd, chessMove.myEnd) && newPiece == chessMove.newPiece;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(myStart, myEnd, newPiece);
     }
 }
