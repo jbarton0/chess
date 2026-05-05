f4408d32ea833dc5393c208c836e2f49be84237e
jbarton0 <jackienomin@gmail.com>
2026-05-04 23:14:56 -0600

    ChessPiece fully implemented

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,7 @@
 package chess;
 
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -81,5 +82,19 @@ public class ChessPiece {
             return new PawnMovesCalculator().getMoves(board, myPosition);
         }
         return null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPiece that = (ChessPiece) o;
+        return myType == that.myType && myColor == that.myColor;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(myType, myColor);
     }
 }
