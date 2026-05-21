7a7699acab278becf9feb978ca0d62c948458035
jbarton0 <jackienomin@gmail.com>
2026-05-20 21:45:56 -0600

    Small spacing fix

--- server/src/test/java/service/ServiceTests.java
+++ server/src/test/java/service/ServiceTests.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import server.Server;
 import service.request.*;
 import service.result.*;
-
 import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 
