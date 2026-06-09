40c4763eaf71ac30c41f84a1c677cd7416528038
jbarton0 <jackienomin@gmail.com>
2026-06-08 20:48:17 -0600

    Connect ws tests passed

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -1,5 +1,6 @@
 package server.websocket;
 
+import com.google.gson.Gson;
 import org.eclipse.jetty.websocket.api.Session;
 import websocket.messages.ServerMessage;
 
@@ -26,7 +27,7 @@ public class ConnectionManager {
     }
 
     public void broadcast(Session excludeSession, ServerMessage notification) throws IOException {
-        String msg = notification.toString();
+        var msg = new Gson().toJson(notification);
         for (List<Session> lst : connections.values()) {
             for (Session c : lst) {
                 if (c.isOpen()) {
--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -1,21 +1,31 @@
 package server.websocket;
 
 import com.google.gson.Gson;
+import dataaaccess.DataAccessException;
+import dataaaccess.mysqldataaccess.GameDB;
 import io.javalin.websocket.WsCloseContext;
 import io.javalin.websocket.WsCloseHandler;
 import io.javalin.websocket.WsConnectContext;
 import io.javalin.websocket.WsConnectHandler;
 import io.javalin.websocket.WsMessageContext;
 import io.javalin.websocket.WsMessageHandler;
+import model.AuthData;
+import model.GameData;
 import websocket.commands.UserGameCommand;
 import java.io.IOException;
 import org.eclipse.jetty.websocket.api.Session;
 
 import dataaaccess.mysqldataaccess.AuthDB;
+import websocket.messages.ErrorMessage;
+import websocket.messages.LoadGameMessage;
+import websocket.messages.NotificationMessage;
+import websocket.messages.ServerMessage;
 
 public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsCloseHandler {
 
     private final ConnectionManager connections = new ConnectionManager();
+    private final AuthDB authDB = new AuthDB();
+    private final GameDB gameDB = new GameDB();
 
     @Override
     public void handleConnect(WsConnectContext ctx) {
@@ -29,11 +39,11 @@ public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsC
             UserGameCommand command = new Gson().fromJson(ctx.message(), UserGameCommand.class);
             switch (command.getCommandType()) {
                 case CONNECT -> connect(command.getAuthToken(), command.getGameID(), ctx.session);
-                case MAKE_MOVE -> exit(action.visitorName(), ctx.session);
-                case LEAVE -> exit(action.visitorName(), ctx.session);
-                case RESIGN -> exit(action.visitorName(), ctx.session);
+//                case MAKE_MOVE -> exit(action.visitorName(), ctx.session);
+//                case LEAVE -> exit(action.visitorName(), ctx.session);
+//                case RESIGN -> exit(action.visitorName(), ctx.session);
             }
-        } catch (IOException ex) {
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
@@ -43,8 +53,40 @@ public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsC
         System.out.println("Websocket closed");
     }
 
-    private void connect(String auth, Integer gameID, Session session) {
-        connections.add(session);
-        var message =
+    private void connect(String auth, Integer gameID, Session session) throws Exception {
+        if (gameID == null | gameDB.getGame(gameID) == null) {
+            sendError(session, new ErrorMessage(ServerMessage.ServerMessageType.ERROR, "Error: invalid game ID"));
+            return;
+        }
+        connections.add(session, gameID);
+        if (!authDB.findAuth(auth)) {
+            sendError(session, new ErrorMessage(ServerMessage.ServerMessageType.ERROR, "Error: invalid authentication"));
+            return;
+        }
+        AuthData authData = authDB.getAuth(auth);
+        GameData gameData = gameDB.getGame(gameID);
+        sendLoadGame(session, new LoadGameMessage(ServerMessage.ServerMessageType.LOAD_GAME, gameData));
+        sendJoined(session, gameData, authData);
+    }
+
+    private void sendLoadGame(Session session, LoadGameMessage msg) throws IOException {
+        var ms = new Gson().toJson(msg);
+        session.getRemote().sendString(ms);
+    }
+
+    private void sendJoined(Session session, GameData gameData, AuthData authData) throws IOException {
+        String message;
+        if (gameData.whiteUsername().equals(authData.username())) {
+            message = String.format("%s has joined the game as WHITE", authData.username());
+        } else {
+            message = String.format("%s has joined the game as BLACK", authData.username());
+        }
+        var notification = new NotificationMessage(ServerMessage.ServerMessageType.NOTIFICATION, message);
+        connections.broadcast(session, notification);
+    }
+
+    private void sendError(Session session, ErrorMessage msg) throws Exception{
+        var ms = new Gson().toJson(msg);
+        session.getRemote().sendString(ms);
     }
 }
